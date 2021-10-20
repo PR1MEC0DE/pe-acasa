b31ddba1e412518871c790465779abb8e9626a24
PR1MEC0DE <92052172+PR1MEC0DE@users.noreply.github.com>
2021-10-20 13:12:59 +0300

    Add files via upload

---
+++ pe acasa.py
@@ -0,0 +1,14 @@
+x = 113
+y = 76
+z = 40
+print("Introdu numarul de barbati la nunta")
+x1 = int(input())
+print("Introdu numarul de femei la nunta")
+y1 = int(input())
+print("Introdu numarul de copii la nunta")
+z1 = int(input())
+a = 455
+c =((x1*x)+(y1*y)+(z1*z))
+b = (c/1000)
+d = (b//0.455)
+print("La nunta este nevoie de", d, ("paini"))
